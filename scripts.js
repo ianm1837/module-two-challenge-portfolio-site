2328badebcc7075ac2c520bf33b19a3e8e4eb88c
ianm1837 <ian@iwm.dev>
2022-10-30 15:26:39 -0700

    Created button animations and scroll snapping

---
+++ scripts.js
@@ -0,0 +1,30 @@
+function navScroller(position){
+	document.getElementById(position).scrollIntoView({block: 'center' })
+}
+
+const main = document.querySelector("#main");
+const cards = document.querySelectorAll(".card");
+const navButton = document.querySelectorAll(".button");
+
+function onScroll() {
+	
+  let current = "";
+  cards.forEach((card) => {
+    const sectionTop = card.offsetTop;
+    const sectionHeight = card.clientHeight;
+
+	console.log('section top' + sectionTop);
+	console.log('scrollY' + main.scrollTop);
+    if (main.scrollTop >= sectionTop - sectionHeight / 3) {
+      current = card.getAttribute("id");
+    }
+  });
+
+  navButton.forEach((button) => {
+    button.classList.remove("button-active");
+	
+    if (button.classList.contains(current)) {
+      button.classList.add("button-active");
+    }
+  });
+};
